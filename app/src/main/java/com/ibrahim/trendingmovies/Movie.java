9ec380819f6917c9201ad179cff301f39cc64c06
ibrahim <ibrahimabdellatif186@gmail.com>
2021-08-19 23:35:53 +0200

    add unused variable and it's optional in API

--- app/src/main/java/com/ibrahim/trendingmovies/Movie.java
+++ app/src/main/java/com/ibrahim/trendingmovies/Movie.java
@@ -9,36 +9,48 @@ public class Movie implements Serializable {
     private int id;
     @SerializedName("image")
     private String imagePath;
-    @SerializedName("name")
-    private String name;
-    @SerializedName("date")
-    private String date;
+    @SerializedName("original_title")
+    private String original_title;
+    @SerializedName("release_date")
+    private String release_date;
     @SerializedName("rating")
     private double rating;
     @SerializedName("overview")
     private String overview;
     private List<Movie> movies;
 
+    //more unused variable {it's optional in api}
+    private boolean adult;
+    private List<Integer> genre_ids;
+    private String original_language;
+    private String title;
+    private String backdrop_path;
+    private double popularity;
+    private int vote_count;
+    private boolean video;
+    private double vote_average;
+    private int total_pages;
+    private int total_results;
+
+
     public Movie(List<Movie> movies) {
         this.movies = movies;
     }
 
-
     public List<Movie> getMovies() {
         return movies;
     }
-
 
     public String getImagePath() {
         return "\"https://image.tmdb.org/t/p/w500/\"" + imagePath;
     }
 
     public String getName() {
-        return name;
+        return original_title;
     }
 
     public String getDate() {
-        return date;
+        return release_date;
     }
 
     public double getRating() {
@@ -47,5 +59,49 @@ public class Movie implements Serializable {
 
     public String getOverview() {
         return overview;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public List<Integer> getGenre_ids() {
+        return genre_ids;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBackdrop_path() {
+        return backdrop_path;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public int getVote_count() {
+        return vote_count;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public double getVote_average() {
+        return vote_average;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public int getTotal_results() {
+        return total_results;
     }
 }
